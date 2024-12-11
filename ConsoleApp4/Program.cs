1d8a5775a9eae9e6013cc4629b52b3930de9345b
sasakozukhar <sasakozuhar93@gmail.com>
2024-12-11 08:50:58 +0200

    Add files via upload

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {/// <summary>
+     ///  Дано целое число n, указывающее номер дня недели от 1 до 7. По указанному номеру n вывести название соответствующего дня недели.
+     /// </summary>
+     /// <param name="args"></param>
+     static int Input(string mes)
+            
+        {
+            Console.WriteLine(mes);
+            return int.Parse(Console.WriteLine());
+
+        }
+        static void Main(string[] args)
+        {
+            int day = Input("day=");
+
+           
+
+        }
+        static string GetDay(int day)
+        {
+            switch (day)
+            {
+                case 1:return "понедельник";
+                    case 2:return "вторник";
+                case 3: return "среда";
+                case 4: return "четверг";
+                case 5: return "пятница";
+                case 6: return "суббота";
+                case 7: return "воскресенье";
+
+                default: return "Ошибка";
+            }
+        }
+    }
+}
